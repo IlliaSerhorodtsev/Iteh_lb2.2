c5533e62f9e751e36846b2e9d83d8a78e60c5fe0
Illia Serhorodtsev <Illia.serhorodtsev@nure.ua>
2022-06-03 12:25:48 +0300

    main

--- year.php
+++ year.php
@@ -10,6 +10,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
     <title>Lb2</title>
 </head>
 <body>
@@ -19,13 +20,14 @@
    $fyear = $_POST['FYear'];
    $syear = $_POST['SYear'];
   print "<table border ='1'>";
-    print " <tr><td><b>Назва</td><td><b>Рік</td></tr>";
+    print " <tr><td><b>Назва</td><td><b>Рік</td><td><b>Вид</td></tr>";
     $coll=$tbl->find(["year"=>['$gte' => (int)$fyear,'$lte' => (int)$syear]]);
           
     foreach ($coll as $col) {
       $nam=$col['name'];
       $yea=$col['year'];
-      print " <tr><td>$nam</td><td>$yea</td></tr>";
+      $type=$col['type'];
+      print " <tr><td>$nam</td><td>$yea</td><td>$type</td></tr>";
     
     }
 ?>
